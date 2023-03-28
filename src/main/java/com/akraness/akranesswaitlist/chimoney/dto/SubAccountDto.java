5c40dfc07b56bc744f147550a30972ca02f9efa3
chris imoni <imonireal@gmail.com>
2023-03-28 11:29:31 +0100

    fixed issues

--- src/main/java/com/akraness/akranesswaitlist/chimoney/dto/SubAccountDto.java
+++ src/main/java/com/akraness/akranesswaitlist/chimoney/dto/SubAccountDto.java
@@ -1,8 +1,10 @@
 package com.akraness.akranesswaitlist.chimoney.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class SubAccountDto {
     private String subAccountId;
     private String uid;
